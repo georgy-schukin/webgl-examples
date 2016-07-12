e483d3a29ceb4eee00d4145b15155bf6bcfab719
Georgy Schukin <georgy.schukin@gmail.com>
2016-07-12 18:26:17 +0600

    Add computation of smooth normals to wave example

--- wave/app.js
+++ wave/app.js
@@ -7,6 +7,7 @@ var program;
 var shift = 0.0;
 var waveMeshPointsNum = 101;
 var waveMeshSize = 100.0;
+var useAnalyticalNormals = true;
 
 function initGL() {
 	gl.clearColor(0.0, 0.0, 0.2, 1.0);	
@@ -34,8 +35,7 @@ function initProgram() {
 }
 
 function makeSurface(surfaceFunc, originByX, originByY, numOfPointsByX, numOfPointsByY, stepByX, stepByY) {
-	var vertices = [];
-	var normals = [];	
+	var vertices = [];	
 	for (var i = 0; i < numOfPointsByY; i++) {
 		for (var j = 0; j < numOfPointsByX; j++) {
 			var x = originByX + j*stepByX;
@@ -43,12 +43,7 @@ function makeSurface(surfaceFunc, originByX, originByY, numOfPointsByX, numOfPoi
 			var z = surfaceFunc.f(x, y);
 			vertices.push(x); 
 			vertices.push(z); 
-			vertices.push(y); 			
-			var dfdx = surfaceFunc.dfdx(x, y);
-			var dfdy = surfaceFunc.dfdy(x, y);								
-			normals.push(-dfdx);
-			normals.push(1.0);
-			normals.push(-dfdy);
+			vertices.push(y); 						
 		}
 	}		
 
@@ -65,10 +60,77 @@ function makeSurface(surfaceFunc, originByX, originByY, numOfPointsByX, numOfPoi
 	}		
 
 	return {
-		vertices: vertices,		
-		normals: normals,
+		vertices: vertices,				
 		indices: indices
 	}	
+}
+
+function generateAnalyticNormals(surfaceFunc, vertices) {
+	var normals = [];
+	for (var i = 0; i < vertices.length; i += 3) {
+		var x = vertices[i];
+		var y = vertices[i + 2];
+		var dfdx = surfaceFunc.dfdx(x, y);
+		var dfdy = surfaceFunc.dfdy(x, y);								
+		normals.push(-dfdx);
+		normals.push(1.0);
+		normals.push(-dfdy);
+	}
+	return normals;
+}
+
+function generateSmoothNormals(vertices, indices) {	
+	// Find which triangles belong to which vertices.
+	var trianglesPerVertex = [];	
+	for (var i = 0; i < indices.length; i++) {
+		var triangleIndex = Math.floor(i/3);
+		var index = indices[i];	
+		if (!(index in trianglesPerVertex)) {
+			trianglesPerVertex[index] = [];
+		}
+		trianglesPerVertex[index].push(triangleIndex);
+	}
+	// Compute one normal for each triangle.
+	var normalsPerTriangle = [];
+	var numOfTriangles = Math.floor(indices.length / 3);
+	for (var tIndex = 0; tIndex < numOfTriangles; tIndex++) {		
+		var normal = computeNormal(vertices, 
+			indices[3*tIndex], 
+			indices[3*tIndex + 1], 
+			indices[3*tIndex + 2]);
+		normalsPerTriangle[tIndex] = normal;
+	}
+	// Compute one smooth normal for each vertex.
+	var normals = [];
+	var numOfVertices = Math.floor(vertices.length / 3);
+	for (var vIndex = 0; vIndex < numOfVertices; vIndex++) {
+		var triangles = trianglesPerVertex[vIndex];
+		var normal = vec3.fromValues(0.0, 0.0, 0.0);		
+		triangles.forEach( function (tIndex) {
+			vec3.add(normal, normal, normalsPerTriangle[tIndex]);
+		});
+		normals.push(normal[0]);
+		normals.push(normal[1]);
+		normals.push(normal[2]);
+	}	
+	return normals;
+}
+
+function computeNormal(vertices, i1, i2, i3) {	
+	var x1 = vertices[3*i1];
+	var y1 = vertices[3*i1 + 1];
+	var z1 = vertices[3*i1 + 2];
+	var x2 = vertices[3*i2];
+	var y2 = vertices[3*i2 + 1];
+	var z2 = vertices[3*i2 + 2];
+	var x3 = vertices[3*i3];
+	var y3 = vertices[3*i3 + 1];
+	var z3 = vertices[3*i3 + 2];
+	var v1 = vec3.fromValues(x2 - x1, y2 - y1, z2 - z1);
+	var v2 = vec3.fromValues(x3 - x1, y3 - y1, z3 - z1);	
+	var cross = vec3.create();
+	vec3.cross(cross, v1, v2);
+	return cross;
 }
 
 function makeWave(meshSize, numOfPoints, shift) {
@@ -88,6 +150,11 @@ function makeWave(meshSize, numOfPoints, shift) {
 		-meshSize/2, -meshSize/2, 
 		numOfPoints, numOfPoints, 
 		meshSize/(numOfPoints - 1), meshSize/(numOfPoints - 1));		
+	if (useAnalyticalNormals) {
+		wave.normals = generateAnalyticNormals(surfaceFunc, wave.vertices);
+	} else {
+		wave.normals = generateSmoothNormals(wave.vertices, wave.indices);
+	}
 	return wave;
 }
 
@@ -212,4 +279,8 @@ function setMeshSize(size) {
 
 function setMeshPoints(num) {
 	waveMeshPointsNum = num;
+}
+
+function toggleAnalyticalNormals(use) {
+	useAnalyticalNormals = use;
 }
